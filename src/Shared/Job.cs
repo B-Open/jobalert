8f92c6c60d1b73b40652ce907fb8768a90fb4a99
Syahnur <syahnurnizam197@gmail.com>
2021-10-23 15:59:16 +0800

    add jobcentre scraper

---
+++ src/Shared/Job.cs
@@ -0,0 +1,17 @@
+namespace Shared
+{
+    public class Job
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string Salary { get; set; }
+
+        public string Location { get; set; }
+
+        public string JobDescription { get; set; }
+    }
+}
