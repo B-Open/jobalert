e29a6d9472de3a3aba47502dfbb97aa49d76efa4
Desmond Chin <chin.sy.des@gmail.com>
2021-11-07 09:45:41 +0800

    remove unnecessary code

--- src/Shared/Utils.cs
+++ src/Shared/Utils.cs
@@ -7,10 +7,6 @@ namespace Shared
 
         public static decimal ConvertKToThousand(string number)
         {
-            bool result = number.ToLower().Contains("k");
-
-            if (!result) return decimal.Parse(number);
-
             return (decimal.Parse(number.ToLower().Replace("k",""))) * 1000;
         }
     }
