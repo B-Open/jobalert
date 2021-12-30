c9c2ccc0c848a69ef3608123095671d18b6ab078
Desmond Chin <chin.sy.des@gmail.com>
2021-12-30 17:38:36 +0800

    fix wrong if check

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -34,7 +34,7 @@ Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
