1e4731e89b3d17a3c6b762af5d9a8d68e0ffba6a
Desmond Chin <chin.sy.des@gmail.com>
2021-12-30 17:30:48 +0800

    use new .NET 6 hosting model

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,26 +1,54 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+using System.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using MySql.Data.MySqlClient;
+using Shared.Repositories;
 
-namespace Api
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddCors();
+
+// add database dependency injection
+var connectionString = builder.Configuration.GetConnectionString("Default");
+services.AddScoped<IDbConnection>(s => new MySqlConnection(connectionString));
+services.AddScoped<IDbTransaction>((s) =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    var conn = s.GetService<IDbConnection>();
+    conn.Open();
+    return conn.BeginTransaction();
+});
+services.AddScoped<IJobRepository, JobRepository>();
+services.AddScoped<ICompanyRepository, CompanyRepository>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+services.AddControllers();
+services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
+});
+
+Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 }
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
